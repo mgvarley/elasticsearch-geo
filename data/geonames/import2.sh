d48440f32cb5e834c367d536128f3f43c7498745
mgvarley <b03d66df56994e4e0f2534fa616cdb06b97896e0>
2016-08-30 11:04:50 +0200

    Added simple geojson app example

--- data/geonames/import2.sh
+++ data/geonames/import2.sh
@@ -7,7 +7,7 @@ echo '{
     "jdbc" : {
         "driver" : "org.xbib.jdbc.csv.CsvDriver",
         "url" : "jdbc:xbib:csv:/Users/markvarley/git/elasticsearch-geo/data/geonames?separator=\t&quotechar=|&columnTypes=String,String,String,String,Double,Double,String,String,String,String,String,String,String,String,Int,Int,String,String,String",
-        "index" : "cities15000",
+        "index" : "cities",
         "type" : "city",
         "sql" : "select geonameid as _id, name, asciiname, alternatenames, latitude as \"location.lat\", longitude as \"location.lon\", feature_class, feature_code, country_code, cc2, admin1_code, admin2_code, admin3_code, admin4_code, population, elevation, dem, timezone, modification_date from cities15000",
         "ignore_null_values": true
---
+++ geojson/index.js
@@ -0,0 +1,59 @@
+// Import the official elastisearch library
+var elasticsearch = require('elasticsearch');
+
+// Setup the client
+var client = new elasticsearch.Client({
+  host: 'localhost:9200'
+});
+
+// Execute the search
+client.search({
+  index: 'cities',
+  type: 'city',
+  body: {
+    // Only pull through the fields we need
+    _source: { include: ['name','location'] },
+    // Match eveything within 25km of the WCCB, Bonn
+    query: {
+      bool: {
+        must: {
+          match_all: {}
+        },
+        filter: {
+          geo_distance: {
+            distance: '25km',
+            location: {
+              lat: 50.719151,
+              lon: 7.124129
+            }
+          }
+        }
+      }
+    }
+  }
+}).then(function (resp) {
+
+  // Setup an empty FeatureCollection object
+  var FeatureCollection = { type: 'FeatureCollection', features: [] }
+
+  // Convert the hits to GeoJson
+  FeatureCollection.features = resp.hits.hits.map(function(hit) {
+    return {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [hit._source.location.lon, hit._source.location.lat]
+      },
+      properties: {
+        name: hit._source.name
+      }
+    }
+  })
+
+  // Pretty print the results to the console
+  // Note: these could be displayed on a map, test the results at http://geojsonlint.com/
+  console.log(JSON.stringify(FeatureCollection, null, 2));
+
+}, function (err) {
+  console.trace(err.message);
+});
